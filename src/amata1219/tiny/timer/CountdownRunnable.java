cade2c9d42e07da8dbf49b4a571bc5275977fd86
amata1219 <suzukiamata@gmail.com>
2018-12-04 21:34:04 +0900

    committed by amata1219.

--- src/amata1219/tiny/timer/CountdownRunnable.java
+++ src/amata1219/tiny/timer/CountdownRunnable.java
@@ -4,8 +4,11 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class CountdownRunnable extends BukkitRunnable {
 
+	private int seconds;
+
 	@Override
 	public void run() {
+		seconds--;
 	}
 
 }
